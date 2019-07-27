50fb948d9f30d6b8a572101a2b035d1b361a55ad
volkangurbuz <volkangurbuz16@gmail.com>
2019-07-27 16:16:29 +0200

    executing runnable with an executor

--- app/src/main/java/com/example/foodrecipes/util/Constants.java
+++ app/src/main/java/com/example/foodrecipes/util/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 
   public static final String BASE_URL = "https://www.food2fork.com/";
   public static final String API_KEY = "5261eada0a051f797c3610ab88800e43";
+  public static final int NETWORK_TIMEOUT = 3000;
 }
