aae6d33eb1f6b0387d90a5527f373f02a6ecede6
volkangurbuz <volkangurbuz16@gmail.com>
2019-09-22 12:44:39 +0200

    added recipe details

--- app/src/main/java/com/example/foodrecipes/RecipeActivity.java
+++ app/src/main/java/com/example/foodrecipes/RecipeActivity.java
@@ -1,5 +1,7 @@
 package com.example.foodrecipes;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.foodrecipes.models.Recipe;
+import com.example.foodrecipes.viewmodels.RecipeViewModel;
 
 public class RecipeActivity extends BaseActivity {
 
@@ -18,6 +21,7 @@ public class RecipeActivity extends BaseActivity {
   private TextView mRecipeTitle, mRecipeRank;
   private LinearLayout mRecileIngredientsContainer;
   private ScrollView mScrollView;
+  private RecipeViewModel mRecipeViewModel;
 
   private static final String TAG = "RecipeActivity";
 
@@ -25,18 +29,46 @@ public class RecipeActivity extends BaseActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_recipe);
-    getIncomingIntent();
     mRecipeImage = findViewById(R.id.recipe_image);
     mRecipeTitle = findViewById(R.id.recipe_title);
     mRecipeRank = findViewById(R.id.recipe_social_score);
     mRecileIngredientsContainer = findViewById(R.id.ingredients_container);
     mScrollView = findViewById(R.id.parent);
+
+    mRecipeViewModel = ViewModelProviders.of(this).get(RecipeViewModel.class);
+
+    subscribeObserver();
+    getIncomingIntent();
   }
 
   private void getIncomingIntent() {
     if (getIntent().hasExtra("recipe")) {
       Recipe recipe = getIntent().getParcelableExtra("recipe");
       Log.d(TAG, "getincomingIntent" + recipe.getTitle());
+      Log.d(TAG, "getincomingIntent" + recipe.getRecipe_id());
+
+      mRecipeViewModel.searchRecipeById(recipe.getRecipe_id());
     }
+  }
+
+  private void subscribeObserver() {
+    mRecipeViewModel
+        .getRecipe()
+        .observe(
+            this,
+            new Observer<Recipe>() {
+              @Override
+              public void onChanged(@Nullable Recipe recipe) {
+
+                Log.d(TAG, "ON CHANGED ============");
+                Log.d(TAG, recipe.getTitle());
+
+                for (String ingredient : recipe.getIngredients()) {
+
+                  Log.d(TAG, ingredient);
+                  Log.d(TAG, "ON CHANGED ============");
+                }
+              }
+            });
   }
 }
